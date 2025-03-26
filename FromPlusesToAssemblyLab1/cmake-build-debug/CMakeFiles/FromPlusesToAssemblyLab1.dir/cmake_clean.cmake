6c63b7b9099fbf31ea7f268a69fe70e04377294f
Vladimir Itmshikov <72025613+FAST-Technologies@users.noreply.github.com>
2025-03-26 13:53:48 +0700

    as

---
+++ FromPlusesToAssemblyLab1/cmake-build-debug/CMakeFiles/FromPlusesToAssemblyLab1.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/FromPlusesToAssemblyLab1.dir/ConstantTableV2_0/ConstantTableV2_0.cpp.obj"
+  "CMakeFiles/FromPlusesToAssemblyLab1.dir/ConstantTableV2_0/ConstantTableV2_0.cpp.obj.d"
+  "CMakeFiles/FromPlusesToAssemblyLab1.dir/OutputModuleV2_0/OutputModuleV2_0.cpp.obj"
+  "CMakeFiles/FromPlusesToAssemblyLab1.dir/OutputModuleV2_0/OutputModuleV2_0.cpp.obj.d"
+  "CMakeFiles/FromPlusesToAssemblyLab1.dir/Scanner/Scanner.cpp.obj"
+  "CMakeFiles/FromPlusesToAssemblyLab1.dir/Scanner/Scanner.cpp.obj.d"
+  "CMakeFiles/FromPlusesToAssemblyLab1.dir/VariableTableV2_0/VariableTableV2_0.cpp.obj"
+  "CMakeFiles/FromPlusesToAssemblyLab1.dir/VariableTableV2_0/VariableTableV2_0.cpp.obj.d"
+  "CMakeFiles/FromPlusesToAssemblyLab1.dir/test_main.cpp.obj"
+  "CMakeFiles/FromPlusesToAssemblyLab1.dir/test_main.cpp.obj.d"
+  "FromPlusesToAssemblyLab1.exe"
+  "FromPlusesToAssemblyLab1.exe.manifest"
+  "FromPlusesToAssemblyLab1.pdb"
+  "libFromPlusesToAssemblyLab1.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/FromPlusesToAssemblyLab1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- FromPlusesToAssemblyLab1/cmake-build-debug/cmake_install.cmake
+++ FromPlusesToAssemblyLab1/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/Vladimir/FromPlussesToAssembly/FromPlusesToAssemblyLab1
+# Install script for directory: E:/filedAssemb/FromPlussesToAssembly/FromPlusesToAssemblyLab1
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/Vladimir/FromPlussesToAssembly/FromPlusesToAssemblyLab1/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "E:/filedAssemb/FromPlussesToAssembly/FromPlusesToAssemblyLab1/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
